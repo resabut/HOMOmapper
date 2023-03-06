febf5e75426c49dda0d18723eca77a04a3238e4a
Joan Escriva Font <esfontjo@gmail.com>
2023-03-06 16:10:26 +0100

    Python script is now able to sort the snp data correctly

--- pytohn_script.py
+++ pytohn_script.py
@@ -9,6 +9,17 @@ indiv_map = pd.read_table("Data/Example1/indiv.map", sep="\t", header=None)
 
 # sort cell content
 sort_indiv_ped = indiv_ped.applymap(lambda x: ''.join(sorted(x)) if isinstance(x, str) else x)
+sort_indiv_peds = sort_indiv_ped
+sort_indiv_peds.iloc[:,6:]= indiv_ped.iloc[:,6:].applymap(lambda x: ''.join(sorted(x)) if isinstance(x, str) else x)
 
 #crate a proper function
+
+
+def order_snp (df):
+
+    df.iloc[:,6:] = df.iloc[:,6:].applymap(lambda x: ''.join(sorted(x)).strip() if isinstance(x, str) else x)
+    return df
+
+sorted = order_snp(indiv_ped)
+a= sorted.dtypes
 # now apply them only to the snp columns
