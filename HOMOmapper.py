69c6c09545d1856e627cdaebcf10606ea0822ab4
Joan Escriva Font <esfontjo@gmail.com>
2023-03-08 10:44:53 +0100

    Added find_roh function to HOMOmapper.py. It doesn't work yet.

--- HOMOmapper.py
+++ HOMOmapper.py
@@ -1,20 +1,21 @@
 """
 info
 """
-
+import time
 import pandas as pd  # 1.5.3
+
 # remove false positve warning related to chained assignment
 pd.options.mode.chained_assignment = None  # default='warn'
-import time
+
 
 # config
 # minimum length of ROH - it used for the window scanning
 min_roh = 15
 # max number of mismatches
-max_mismatch = 2
+max_mismatch = 3
 # for testing, shorten ped files to 50 cols
 short = True
-short_len = 50
+short_len = 10000
 
 print("Loading data...")
 
@@ -43,7 +44,9 @@ indiv_ped.rename(index={0: "Ind"}, inplace=True)
 all_data_df = pd.concat([chr_row, indiv_ped, ref_ped], axis=0)
 # make temporary shorter ones, if preferred
 if short:
+    print("Shortening data...")
     all_data_df = all_data_df.iloc[:, 0:short_len]
+
 
 # this function sorts the snp column content
 def order_snp(df):
@@ -52,19 +55,17 @@ def order_snp(df):
     # creates genotype cells
     # ignores the first 6 columns
     # the rest of the columns are sorted to  eliminate genotype 13 and 31 ambiguity
-    df[df.columns[6:]] = df[df.columns[6:]].applymap(lambda x: int(''.join(sorted(x)).strip()) if isinstance(x, str) else x)
+    df[df.columns[6:]] = df[df.columns[6:]].applymap(
+        lambda x: int(''.join(sorted(x)).strip()) if isinstance(x, str) else x)
     # returns the dataframe with the genotype data where each column is a loci
     end_time = time.time()
     print(f"Time to sort data: {end_time - start_time:.4f}")
     return df
 
 
-
 # sort the ped files
 
 all_data_df.iloc[1:, :] = order_snp(all_data_df.iloc[1:, :])
-
-
 
 
 # find matches
@@ -75,9 +76,9 @@ def find_matches(df):
     # find matches
     print("Finding matches...")
     start_time = time.time()
-    for pair in range(2, len(df.index)): # number of pairwise comparisons
+    for pair in range(2, len(df.index)):  # number of pairwise comparisons
         # print(f"Pair {pair}")
-        for snp in range(6, len(df.columns)): # number of snp
+        for snp in range(6, len(df.columns)):  # number of snp
             # print(f"SNP number {snp}")
             if df.iloc[1, snp] == df.iloc[pair, snp]:
                 # match is 0
@@ -89,5 +90,30 @@ def find_matches(df):
 
 matches_df = find_matches(all_data_df)
 
+
 # find homologous regions
 def find_roh(df):
+    print("Finding ROH...")
+    start_time = time.time()
+    for pair in range(2, len(df.index)):
+        print("Pair with individual", pair)
+        for chr_nr in df.iloc[0, 6:].unique().tolist():
+            print(df.iloc[0, 6:].unique().tolist())
+            print("Chromosome", chr_nr)
+            # slice by chromosome
+            chr_df = df.iloc[df.iloc[0, :] == chr_nr]
+            print(chr_df)
+            num_loci = len(chr_df.columns[6:])
+            print("Number of loci", num_loci)
+            for start_pos in range(0, num_loci - min_roh):
+                # print("Start position", start_pos)
+                window = chr_df.iloc[pair, start_pos:start_pos + min_roh]
+                print(window)
+                if window.sum() <= max_mismatch:  # mismatch limit
+                    print("Found ROH")
+                    print("Start position", start_pos)
+    end_time = time.time()
+    print(f"Time to find ROH: {end_time - start_time:.4f}")
+
+
+find_roh(matches_df)
